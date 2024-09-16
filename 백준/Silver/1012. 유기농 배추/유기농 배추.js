65c17e012486106c3e27078c887c823df8f5878a
yubin <cyoobin@naver.com>
2024-09-16 15:49:32 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 12172 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -2,64 +2,46 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const t = +input.shift();
-const testCase = [];
+let m, n, k, map;
+const answer = [];
 
 for (let i = 0; i < t; i += 1) {
-  const [m, n, k] = input.shift().split(" ").map(Number);
-
-  const map = Array.from({ length: m }, () =>
-    Array.from({ length: n }, () => 0)
-  );
+  [m, n, k] = input.shift().split(" ").map(Number);
+  map = Array.from({ length: m }, () => Array(n).fill(0));
 
   for (let j = 0; j < k; j += 1) {
     const [x, y] = input.shift().split(" ").map(Number);
     map[x][y] = 1;
   }
 
-  testCase.push(map);
-}
+  let sectionCnt = 0;
 
-function solution(testCase) {
-  const answer = [];
-
-  testCase.forEach((map) => {
-    const currentMap = [...map];
-    const m = map.length;
-    const n = map[0].length;
-    let count = 0;
-
-    for (let i = 0; i < m; i += 1) {
-      for (let j = 0; j < n; j += 1) {
-        if (currentMap[i][j] === 1) {
-          count += 1;
-
-          const dx = [-1, 0, 1, 0];
-          const dy = [0, -1, 0, 1];
-
-          const queue = [[i, j]];
-
-          while (queue.length > 0) {
-            const [x, y] = queue.shift();
-
-            for (let i = 0; i < 4; i += 1) {
-              const nx = x + dx[i];
-              const ny = y + dy[i];
-
-              if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-
-              if (currentMap[nx][ny] === 1) {
-                currentMap[nx][ny] = 0;
-                queue.push([nx, ny]);
-              }
-            }
-          }
-        }
+  for (let x = 0; x < m; x += 1) {
+    for (let y = 0; y < n; y += 1) {
+      if (map[x][y] === 1) {
+        sectionCnt += 1;
+        map[x][y] = 0;
+        dfs(x, y);
       }
     }
-    answer.push(count);
-  });
+  }
 
-  return answer.join("\n");
+  answer.push(sectionCnt);
 }
 
-console.log(solution(testCase));
+function dfs(x, y) {
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+
+  for (let i = 0; i < 4; i += 1) {
+    const nx = x + dx[i];
+    const ny = y + dy[i];
+
+    if (nx < 0 || nx >= m || ny < 0 || ny >= n || map[nx][ny] === 0) continue;
+
+    map[nx][ny] = 0;
+    dfs(nx, ny);
+  }
+}
+
+console.log(answer.join("\n"));
