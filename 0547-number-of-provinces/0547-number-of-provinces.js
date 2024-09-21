c49641ec56216fdc992c6e3286213273708c5989
yubin <cyoobin@naver.com>
2024-09-21 16:31:02 +0900

    Time: 63 ms (52.08%), Space: 51.8 MB (51.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.js
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -4,40 +4,25 @@
  */
 const findCircleNum = function(isConnected) {
     const n = isConnected.length;
-    const visited = Array.from({ length: n }, () => Array(n).fill(false));
+    const visited = Array(n).fill(false);
     let answer = 0;
 
     for (let i = 0; i < n; i += 1) {
-        for (let j = 0; j < n; j += 1) {
-            if (isConnected[i][j] === 1 && !visited[i][j]) {
-                answer += 1;
-                visited[i][j] = true;
-
-                BFS(isConnected, visited, n, i, j);
-            }
+        if (!visited[i]) {
+            answer += 1;
+            DFS(i, isConnected, visited)
         }
     }
 
     return answer;
 };
 
-function BFS(isConnected, visited, n, x, y) {
-    const dx = [1, 0, -1, 0];
-    const dy = [0, 1, 0, -1];
+const DFS = function(node, isConnected, visited) {
+    visited[node] = true;
 
-    const queue = [[x, y]];
-
-    while (queue.length) {
-        const [x, y] = queue.shift();
-
-        for (let i = 0; i < 4; i+= 1) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-
-            if (nx < 0 || nx >= n ||  ny < 0 || ny >= n || visited[nx][ny] || isConnected[nx][ny] === 0) continue;
-
-            visited[nx][ny] = true;
-            queue.push([nx, ny]);
+    for (let i = 0; i < isConnected.length; i+= 1) {
+        if (isConnected[node][i] === 1 && !visited[i]) {
+            DFS(i, isConnected, visited);
         }
     }
 }
