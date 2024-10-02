3863ac57c4e2304b78bc0f738d5cbf7e3d5f892c
yubin <cyoobin@naver.com>
2024-10-02 10:39:19 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = 0;
+    let num = n;
+    
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+            answer += 1;
+        }
+    }
+    
+    return answer;
+}
