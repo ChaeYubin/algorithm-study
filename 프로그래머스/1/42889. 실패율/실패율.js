6df5ccef4fad021190e0a1de1afab5e4959b93c9
yubin <cyoobin@naver.com>
2025-01-03 20:23:05 +0900

    [level 1] Title: 실패율, Time: 7.80 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,27 +1,31 @@
 function solution(N, stages) {
-    const challenge = new Array(stages.length).fill(0);
-    const failure = new Array(stages.length).fill(0);
-    const rate = [];
-    const answer = [];
-    
-    stages.forEach(num => {
-        for (let i = 0; i < num; i++) {
-            challenge[i] += 1;
-        }
-        failure[num - 1] += 1;
-    })
-    
-    for (let i = 0; i < N; i++) {
-        rate.push({num: i+1, rate: (failure[i] === 0 ? 0 : failure[i] / challenge[i])})
+    // 스테이지 별 도전자 수 구하기(특정 단계에 머물러 있다 = 실패)
+    const challenger = new Array(N + 2).fill(0);
+    for (const stage of stages) {
+        challenger[stage] += 1;
     }
     
-    const sortedRate = rate.sort((a, b) => {
-        if (a.rate === b.rate) {
-            return a.num - b.num;
-        } else {
-            return b.rate - a.rate;
-        }
-    });
+    // 실패한 사용자 수 계산
+    const fails = {};
+    let total = stages.length;
     
-    return sortedRate.map((v) => parseInt(v.num))
+    // 각 스테이지를 순회하며, 실패율 계산
+    for (let i = 1; i <= N; i++) {
+        // 도전한 사람이 없는 경우, 실패율은 0
+        if (challenger[i] === 0) {
+            fails[i] = 0;
+            continue;
+        }
+        
+        // 실패율 계산
+        fails[i] = challenger[i] / total;
+        
+        // 다음 스테이지의 실패율을 구하기 위해 현재 스테이지의 인원을 뺌
+        total -= challenger[i];
+    }
+    
+    // 실패율이 높은 스테이지부터 내림차순으로 정렬
+    const result = Object.entries(fails).sort((a, b) => b[1] - a[1]);
+    
+    return result.map((v) => Number(v[0]));
 }
