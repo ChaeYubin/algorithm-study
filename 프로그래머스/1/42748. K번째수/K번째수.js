f8b2d29619088bfe83ec58358609bfcf38f62c96
yubin <cyoobin@naver.com>
2025-01-04 20:33:00 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    const answer = [];
+    
+    for (let [s, e, k] of commands) {
+        answer.push(array.slice(s - 1, e).sort((a, b) => a - b)[k - 1]);
+    }
+    
+    return answer;
+}
