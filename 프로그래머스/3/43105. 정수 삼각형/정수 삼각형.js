085820a6aad8eba67e4ef9d03003d23af34040ce
yubin <cyoobin@naver.com>
2025-01-04 21:48:47 +0900

    [level 3] Title: 정수 삼각형, Time: 7.06 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,24 +1,17 @@
 function solution(triangle) {
-    // let dp = Array.from({ length: triangle.length }, (_, i) => Array.from({ length: i + 1 }, () => 0));
-    // 위처럼 하지 않고 1차원 배열 하나만 사용해서 공간복잡도를 낮출 수 있다.
+    const n = triangle.length;
+    const dp = Array.from({ length: n },() => Array(n).fill(0));
     
-    let prev = triangle[0]; // 첫 번째 행으로 초기화
-    
-    for (let i = 1; i < triangle.length; i += 1) {
-        const current = Array(i + 1).fill(0); // 현재 행을 위한 배열
-        
-        for (let j = 0; j < triangle[i].length; j += 1) {
-            if (j === 0) {
-                current[j] = prev[j] + triangle[i][j]; // 첫 번째 원소
-            } else if (j === i) {
-                current[j] = prev[j-1] + triangle[i][j]; // 마지막 원소
-            } else {
-                current[j] = Math.max(prev[j-1] , prev[j]) + triangle[i][j];
-            }
-        }
-        
-        prev = current; // 현재 행을 이전 행으로 업데이트
+    // dp 테이블의 맨 아래쪽 라인 초기화
+    for (let i = 0; i < n; i++) {
+        dp[n - 1][i] = triangle[n - 1][i];
     }
     
-    return Math.max(...prev);
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+        }
+    }
+    
+    return dp[0][0];
 }
