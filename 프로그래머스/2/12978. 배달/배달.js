37afb04a4757bdc8dc3caec9668d897e1551465a
yubin <cyoobin@naver.com>
2025-01-04 13:19:22 +0900

    [level 2] Title: 배달, Time: 4.87 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,12 +1,11 @@
 function solution(N, road, K) {
     const graph = Array.from({length: N + 1}, () => []);
     const dist = Array(N + 1).fill(Infinity);
-
-    road.forEach((value) => {
-        let [start, end, cost] = value;
+    
+    road.forEach(([start, end, cost]) => {
         graph[start].push({to: end, dist: cost});
         graph[end].push({to: start, dist: cost});
-    })
+    });
     
     const queue = [{to: 1, dist: 0}];
     dist[1] = 0;
@@ -17,12 +16,10 @@ function solution(N, road, K) {
         graph[to].forEach((next) => {
             if (dist[next.to] > dist[to] + next.dist) {
                 dist[next.to] = dist[to] + next.dist;
-                // 최단 경로가 되는 노드는 큐에 추가
                 queue.push(next);
             }
-        });
+        })
     }
     
-    const answer = dist.filter((v) => v <= K).length;
-    return answer;
+    return dist.filter((v) => v <= K).length;
 }
