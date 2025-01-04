b8d3c0a70cc318b2f0f7ba906c16ba213fe97a86
yubin <cyoobin@naver.com>
2025-01-04 13:00:40 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,32 +1,24 @@
+function dfs(computers, visited, node) {
+    visited[node] = true; // 현재 노드 방문 처리
+    
+    for (let i = 0; i < computers[node].length; i++) {
+        if (computers[node][i] && !visited[i]) {
+            dfs(computers, visited, i);
+        }
+    }
+}
+
 function solution(n, computers) {
     let visited = Array(n).fill(false);
     
-    let network = 0;
-    // BFS로 탐색 -> 방문하지 않았던 지점을 시작으로 연결되어 있는 만큼 계속 돈다.
-    
-    const bfs = (index) => {
-        const queue = [index];
-        
-        while (queue.length) {
-            const curIndex = queue.shift();
-            visited[curIndex] = true;
-            
-            for (let i = 0; i < n; i += 1) {
-                if (i === curIndex) continue;
-                
-                if (computers[curIndex][i] && !visited[i]) {
-                    queue.push(i);
-                }
-            }
-        }
-    }
+    let answer = 0;
     
     for (let i = 0; i < n; i += 1) {
         if (!visited[i]) {
-            network += 1;
-            bfs(i);
+            dfs(computers, visited, i);
+            answer += 1;
         }
     }
     
-    return network;
+    return answer;
 }
