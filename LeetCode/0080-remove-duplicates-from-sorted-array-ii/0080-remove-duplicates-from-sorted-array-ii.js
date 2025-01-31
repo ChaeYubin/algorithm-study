4720befee5d36002f0eeff1d238d71736ee643eb
yubin <cyoobin@naver.com>
2025-01-31 17:17:43 +0900

    Time: 64 ms (46.5%), Space: 51 MB (97.64%) - LeetHub

---
+++ LeetCode/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let pointer = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] === nums[pointer]) {
+            pointer++;
+        } 
+
+        nums.splice(i + 1, pointer - i - 2);
+        pointer = i + 2;
+    }
+};
