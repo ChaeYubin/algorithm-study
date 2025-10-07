494992bf42d2818851f345df41f81c8839e8e60e
yubin <cyoobin@naver.com>
2025-10-07 11:31:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 63.97 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,19 +1,20 @@
 function solution(elements) {
+    const n = elements.length;
+    const el = elements.concat(elements);
+    const prefix = [0];
+    
+    // 누적합 배열 생성
+    for (let i = 0; i < el.length; i++) {
+        prefix[i + 1] = prefix[i] + el[i];
+    }
+    
     const answer = new Set();
     
-    const el = elements.concat(elements);  // 원형임을 나타내기 위해 이어붙인 배열
-    
-    // 시작이 elements[i]인 연속 부분 수열
-    for (let i = 0; i < elements.length; i += 1) {
-        let p1 = i;
-        let sum = 0;
-        
-        // 길이가 j인 연속 부분 수열
-        for (let j = 0; j < elements.length; j += 1) {
-            let p2 = p1 + j;
-            
-            sum += el[p2];
-            
+    // 부분 수열 길이(1~n)
+    for (let len = 1; len <= n; len++) {
+        for (let start = 0; start < n; start++) {
+            const end = start + len;
+            const sum = prefix[end] - prefix[start];
             answer.add(sum);
         }
     }
