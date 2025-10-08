c6c10402cd7c837917549698cf54bda99348aa27
yubin <cyoobin@naver.com>
2025-10-08 11:44:55 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,37 @@
+function solution(begin, target, words) {
+    const visited = Array(words.length).fill(0);  // 방문 여부 체크 배열(한번 확인했던 단어는 확인할 필요가 없으므로)
+    
+    const queue = [];
+    queue.push([begin, 0]);  // [단어, 단어에 도달한 단계]
+    
+    // 규칙에 따라 변환 가능한지 확인하는 함수
+    const isConvertable = (str1, str2) => {
+        let different = 0;
+        
+        for (let i = 0; i < str1.length; i++) {
+            if (str1[i] !== str2[i]) different++;
+        }
+        
+        return different === 1 ? true : false;
+    }
+    
+    while (queue.length > 0) {
+        const [current, stage] = queue.shift();
+        
+        for (let i = 0; i < words.length; i++) {
+            if (visited[i]) continue;  // 방문한 적 있다면 스킵
+            
+            if (isConvertable(current, words[i])) {
+                if (words[i] === target) {
+                    return stage + 1;  // target을 발견하면 즉시 리턴
+                }
+                
+                // target이 아닌 경우 방문 처리하고 다음 탐색에 포함
+                visited[i] = 1;
+                queue.push([words[i], stage + 1]);
+            }
+        } 
+    }
+  
+    return 0;
+}
