ddd187fb96f38319ef373c5a98461adc4a272f50
yubin <cyoobin@naver.com>
2026-02-15 13:04:01 +0900

    [Silver III] Title: N과 M (4), Time: 88 ms, Memory: 15044 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr;
+
+    static void dfs(int count, int start) {
+        if (count == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            arr[count] = i + 1;
+            dfs(count + 1, i);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+}
