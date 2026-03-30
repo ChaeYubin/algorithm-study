b52395b82d5913747215b9d165d10983b6bd24c9
yubin <cyoobin@naver.com>
2026-03-30 14:59:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 88 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -2,20 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
+// 0-1 BFS 풀이
+// 가중치가 서로 다르므로(0과 1) 일반 BFS는 사용할 수 없다.
+// time을 기준으로 PriorityQueue를 사용할 수도 있지만,
+// 가중치가 0과 1이므로 0-1 BFS를 활용하는 것이 더 효율적이다.
 public class Main {
-  static class State implements Comparable<State> {
+  static class State {
     int pos, time;
 
     State(int pos, int time) {
       this.pos = pos;
       this.time = time;
-    }
-
-    public int compareTo(State o) {
-      return this.time - o.time;
     }
   }
 
@@ -26,36 +26,35 @@ public class Main {
     int n = nextInt();
     int k = nextInt();
 
-    if (n == k) {
-      System.out.println(0);
-      return;
-    }
-
     int[] minTime = new int[100_001];
     Arrays.fill(minTime, Integer.MAX_VALUE);
+    minTime[n] = 0;
 
-    PriorityQueue<State> queue = new PriorityQueue<>();
-    queue.offer(new State(n, 0));
+    ArrayDeque<State> deque = new ArrayDeque<>();
+    deque.offer(new State(n, 0));
 
-    while (!queue.isEmpty()) {
-      State cur = queue.poll();
+    while (!deque.isEmpty()) {
+      State cur = deque.poll();
+
+      if (cur.time > minTime[cur.pos])
+        continue;
 
       if (cur.pos == k)
         break;
 
+      if (cur.pos * 2 < 100_001 && minTime[cur.pos * 2] > cur.time) {
+        minTime[cur.pos * 2] = cur.time;
+        deque.addFirst(new State(cur.pos * 2, cur.time));
+      }
+
       if (cur.pos + 1 < 100_001 && minTime[cur.pos + 1] > cur.time + 1) {
         minTime[cur.pos + 1] = cur.time + 1;
-        queue.offer(new State(cur.pos + 1, cur.time + 1));
+        deque.addLast(new State(cur.pos + 1, cur.time + 1));
       }
 
       if (cur.pos - 1 >= 0 && minTime[cur.pos - 1] > cur.time + 1) {
         minTime[cur.pos - 1] = cur.time + 1;
-        queue.offer(new State(cur.pos - 1, cur.time + 1));
-      }
-
-      if (cur.pos * 2 < 100_001 && minTime[cur.pos * 2] > cur.time) {
-        minTime[cur.pos * 2] = cur.time;
-        queue.offer(new State(cur.pos * 2, cur.time));
+        deque.addLast(new State(cur.pos - 1, cur.time + 1));
       }
     }
 
